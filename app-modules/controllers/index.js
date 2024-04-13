a708024952e5bd3c0e7d8711c57bcf9a9a1d017e
goodsoul-andrew <goodsoul.andrew@mail.ru>
2024-04-13 22:39:30 +0500

    Как будто есть контроллеры

---
+++ app-modules/controllers/index.js
@@ -0,0 +1,6 @@
+const mainController = require("../../_api/endpoint");
+const gameController = require("../../_api/game/endpoint");
+const voteController = require("../../_api/vote/endpoint");
+
+
+module.exports = {mainController, gameController, voteController};
--- app.js
+++ app.js
@@ -1,10 +1,21 @@
 const http = require('http');
 const path = require('path');
 const { staticFile, doEndpoint } = require('./app-modules/http-utils');
+const { mainController, gameController, voteController } = require('./app-modules/controllers');
+
 
 const server = http.createServer((req, res) => {
 	const url = req.url;
 	switch (url) {
+    case "/":
+      mainController(req, res);
+      break;
+    case "/game":
+      gameController(req, res);
+      break;
+    case "/vote":
+      voteController(req, res);
+      break;
 		default:
 			if (path.extname(url) === '') {
 				doEndpoint(req, res);
